7900430e4fcca60fd6245b454adb6198a75dc1e1
Foos-Abdullahi <foocclahicali@gmail.com>
2024-03-25 10:52:50 +0300

    database with  project table commited

--- Construction_MT/Backend/construction_venv/contruction_system/Projects/urls.py
+++ Construction_MT/Backend/construction_venv/contruction_system/Projects/urls.py
@@ -3,4 +3,8 @@ from .import views
 
 urlpatterns = [
 path('', views.getAll),
+path('view/<int:id>', views.getById),
+path('delete/<int:id>', views.delete),
+path('create/', views.create),
+path('update/<int:id>', views.update),
 ]
