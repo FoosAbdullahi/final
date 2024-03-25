8c6089b833581f68a8bd1d2fbae2ea90975bafac
Foos-Abdullahi <foocclahicali@gmail.com>
2024-03-25 12:57:55 +0300

    issue_date attribute added in project table

--- Construction_MT/Backend/construction_venv/contruction_system/Projects/models.py
+++ Construction_MT/Backend/construction_venv/contruction_system/Projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
@@ -8,5 +9,6 @@ class Projects(models.Model):
     budget = models.IntegerField(default=0)
     start_date = models.DateField()
     end_date = models.DateField()
+    issue_date = models.DateField(default=datetime.date.today)
     # client_id
 
